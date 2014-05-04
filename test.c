6e84d4566c3aac77468ad6cc4b61c4b82edb409c
Ahmed <ahmad463@hotmail.com>
2014-05-04 15:07:12 +0930

    Create test function combine line left
    Create test function combine line left and case to test combine in left
    diraction

--- test.c
+++ test.c
@@ -98,6 +98,55 @@ int ttl_vector_shift_line_Right(int i1,int i2,int i3,int i4,char *msg,
 }
 
 
+int line_combine_vector_test(int i1,int i2,int i3,int i4,char *msg,
+                int o1,int o2,int o3,int o4,int (*func)(int,int *,int),int d)
+{
+  int list[4]={i1,i2,i3,i4};
+  if (msg) printf("%s - ",msg); 
+  else {
+    printf("Tilting {%d,%d,%d,%d} left yields {%d,%d,%d,%d} - ",
+             i1,i2,i3,i4,o1,o2,o3,o4);
+  }
+  fflush(stdout);
+  func(4,list,d);
+  if ((list[0]!=o1)||(list[1]!=o2)||(list[2]!=o3)||(list[3]!=o4))
+    {
+      printf("FAILED: {%d,%d,%d,%d} became {%d,%d,%d,%d} instead of"
+             " {%d,%d,%d,%d}\n",
+             i1,i2,i3,i4,list[0],list[1],list[2],list[3],
+             o1,o2,o3,o4);
+      return -1;
+    } 
+  printf("PASSED.\n");
+  return 0;
+  
+}
+ 
+
+int test_combine_line_left()
+{
+  int e=0;
+  e|=ttl_vector_combine_line_left(0,0,0,0,"Empty list is empty after combine line left",0,0,0,0);
+  e|=ttl_vector_combine_line_left(1,2,4,8,"Distinct values don't combine",1,2,4,8);
+  e|=ttl_vector_combine_line_left(1,0,0,0,NULL,1,0,0,0);
+  e|=ttl_vector_combine_line_left(1,1,0,0,NULL,2,0,0,0);
+  e|=ttl_vector_combine_line_left(0,0,1,1,NULL,0,0,2,0);
+  e|=ttl_vector_combine_line_left(0,0,0,1,NULL,0,0,0,1);
+  e|=ttl_vector_combine_line_left(1,0,1,1,NULL,1,0,2,0);
+  e|=ttl_vector_combine_line_left(1,1,0,1,NULL,2,0,0,1);
+  e|=ttl_vector_combine_line_left(1,1,1,1,NULL,2,0,2,0);
+  
+   return e;
+}
+
+
+
+int ttl_vector_combine_line_left(int i1,int i2,int i3,int i4,char *msg,
+               int o1,int o2,int o3,int o4)
+{
+	
+  return line_combine_vector_test(i1,i2,i3,i4,msg,o1,o2,o3,o4,combine_tiles,0);
+}
 
 
 
@@ -107,6 +156,7 @@ int main(int argc,char **argv)
   int e=0;
   e|=test_shift_line_left();
   e|=test_shift_line_Right();
+  e|=test_combine_line_left();
   
   e|=test_tilt_left();
   return e;
