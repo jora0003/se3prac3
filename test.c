8a2d3fe8062b130191ea5baca51de1f606e63964
Ahmed <ahmad463@hotmail.com>
2014-05-04 14:04:56 +0930

    Create test function shift line right
    create  test function test_shift_line_Right() and test case test shift
    line right.

--- test.c
+++ test.c
@@ -74,6 +74,31 @@ int ttl_vector_shift_line_left(int i1,int i2,int i3,int i4,char *msg,
 
 
 
+int test_shift_line_Right()
+{
+  int e=0;
+  e|=ttl_vector_shift_line_Right(0,0,0,0,"Empty list is empty after shift_line_Right",0,0,0,0);
+  e|=ttl_vector_shift_line_Right(0,0,0,1,"Value on Right stays on Right after shift_line_Right",0,0,0,1);
+  e|=ttl_vector_shift_line_Right(1,0,0,0,"Value on left shifts to Right edge after shift_line_Right",0,0,0,1);
+  e|=ttl_vector_shift_line_Right(0,0,1,0,"Value in middle shifts to Right edge after shift_line_Right",0,0,0,1);
+  e|=ttl_vector_shift_line_Right(1,2,4,8,"Distinct values don't combine",1,2,4,8);
+  e|=ttl_vector_shift_line_Right(1,1,1,1,"Combinations don't cascade",1,1,1,1);
+  e|=ttl_vector_shift_line_Right(0,0,1,1,NULL,0,0,1,1);
+  e|=ttl_vector_shift_line_Right(4,0,1,1,NULL,0,4,1,1);
+  e|=ttl_vector_shift_line_Right(0,1,0,1,NULL,0,0,1,1); 
+  return e;
+}
+
+
+int ttl_vector_shift_line_Right(int i1,int i2,int i3,int i4,char *msg,
+               int o1,int o2,int o3,int o4)
+{
+	
+  return line_vector_test(i1,i2,i3,i4,msg,o1,o2,o3,o4,shift_line_Right);
+}
+
+
+
 
 
 
@@ -81,6 +106,7 @@ int main(int argc,char **argv)
 {
   int e=0;
   e|=test_shift_line_left();
+  e|=test_shift_line_Right();
   
   e|=test_tilt_left();
   return e;
