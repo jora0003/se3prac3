91d0fb2cb3b1264df90d69b877f2fe9ffb21f37f
Ahmed <ahmad463@hotmail.com>
2014-05-05 12:58:49 +0930

    Create test function board set column
    This function  test and add cases to change  one column in array 2x2

--- test.c
+++ test.c
@@ -271,6 +271,104 @@ int vector_board_read_column(int in[16], int column, int out[4])
     
 }
 
+ int test_board_set_column()
+{
+   int e = 0;
+   int board[16]=  {0, 0, 2, 0, 
+                    0, 0, 2, 0, 
+                    0, 4, 0, 0, 
+                    1, 0, 2, 0};
+   
+   int column0[4] = {1, 0, 0, 1};
+   int out0[16] = {1, 0, 2, 0,
+                   0, 0, 2, 0,
+				   0, 4, 0, 0,
+				   1, 0, 2, 0};
+   e|= vector_board_set_column(board, 0, column0, out0);
+   
+   int column1[4] = {2, 0, 4, 0};
+   int out1[16] = {0, 2, 2, 0,
+                   0, 0, 2, 0,
+				   0, 4, 0, 0,
+				   1, 0, 2, 0};
+   e|= vector_board_set_column(board, 1, column1, out1);
+   
+   int column2[4] = {2, 2, 8, 2};
+   int out2[16] = {0, 0, 2, 0,
+                   0, 0, 2, 0,
+				   0, 4, 8, 0,
+				   1, 0, 2, 0};
+   e|= vector_board_set_column(board, 2, column2, out2);
+   
+   
+   int column3[4] = {4, 4, 1, 0};
+   int out3[16] = {0, 0, 2, 4,
+                   0, 0, 2, 4,
+				   0, 4, 0, 1,
+				   1, 0, 2, 0};
+   e|= vector_board_set_column(board, 3, column3, out3);
+   
+   return e;
+}
+
+ int vector_board_set_column(int in[16], int column, int columnData[4], int out[16])
+{
+   int **board = calloc(4, sizeof(int *));
+   int i = 0;
+   int j = 0;
+   for(i = 0; i < 4; ++i)
+   {
+      board[i] = calloc(4, sizeof(int));
+      for(j = 0; j < 4; ++j)
+      {
+         int index = 4 * i + j;
+         board[i][j] =  in[index];
+      }
+   }
+   
+
+   printf("Set column %d to {%d,%d,%d,%d} for \n{{%d,%d,%d,%d}\n {%d,%d,%d,%d}\n {%d,%d,%d,%d}\n {%d,%d,%d,%d}}\n yields\n {{%d,%d,%d,%d}\n {%d,%d,%d,%d}\n {%d,%d,%d,%d}\n {%d,%d,%d,%d}} - ",
+             column,
+             columnData[0], columnData[1], columnData[2], columnData[3], 
+             in[0], in[1], in[2], in[3], 
+             in[4], in[5], in[6], in[7],
+             in[8], in[9], in[10], in[11], 
+             in[12], in[13], in[14], in[15],
+             out[0], out[1], out[2], out[3], 
+             out[4], out[5], out[6], out[7],
+             out[8], out[9], out[10], out[11], 
+             out[12], out[13], out[14], out[15]);
+      
+   fflush(stdout);
+   board_set_column(4, board, column, columnData);
+    if ((board[0][0]!=out[0])||(board[0][1]!=out[1])||(board[0][2]!=out[2])|| (board[0][3]!=out[3]) ||
+          (board[1][0]!=out[4])||(board[1][1]!=out[5])||(board[1][2]!=out[6])|| (board[1][3]!=out[7]) ||
+          (board[2][0]!=out[8])||(board[2][1]!=out[9])||(board[2][2]!=out[10])|| (board[2][3]!=out[11]) ||
+          (board[3][0]!=out[12])||(board[3][1]!=out[13])||(board[3][2]!=out[14])|| (board[3][3]!=out[15]) )
+        {
+	     printf("FAILED: Set column %d to {%d,%d,%d,%d} for  {{%d,%d,%d,%d}, {%d,%d,%d,%d}, {%d,%d,%d,%d}, {%d,%d,%d,%d}} returned {{%d,%d,%d,%d}, {%d,%d,%d,%d}, {%d,%d,%d,%d}, {%d,%d,%d,%d}} instead of"
+                 " {{%d,%d,%d,%d}, {%d,%d,%d,%d}, {%d,%d,%d,%d}, {%d,%d,%d,%d}}\n",
+                 column,
+                 columnData[0], columnData[1], columnData[2], columnData[3], 
+                 in[0], in[1], in[2], in[3], 
+                 in[4], in[5], in[6], in[7],
+                 in[8], in[9], in[10], in[11], 
+                 in[12], in[13], in[14], in[15],
+                 board[0][0], board[0][1], board[0][2], board[0][3],
+                 board[1][0], board[1][1], board[1][2], board[1][3],
+                 board[2][0], board[2][1], board[2][2], board[2][3],
+                 board[3][0], board[3][1], board[3][2], board[3][3],
+                 out[0], out[1], out[2], out[3], 
+                 out[4], out[5], out[6], out[7],
+                 out[8], out[9], out[10], out[11], 
+                 out[12], out[13], out[14], out[15]);
+           
+          return -1;
+        } 
+         
+      printf("PASSED.\n");
+      return 0;
+}
  
 int main(int argc,char **argv)
 {
@@ -282,5 +380,6 @@ int main(int argc,char **argv)
   e|=test_tilt_left();
   e|=test_tilt_Right();
   e|=test_board_read_column();
+  e|=test_board_set_column();
   return e;
 }
